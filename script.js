04a173aaada910f2020b908b25c7f203b4f9c741
tanzid-48 <mdtanzid.525@gmail.com>
2026-02-22 01:23:45 +0600

    Add the all dom getElementById and add same id in html and fix same bug

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("hello");
+let interviewList = [];
+let rejectedList = [];
+
+let total = document.getElementById("total-count");
+// console.log(total);
+let interviewCount = document.getElementById("interview-count")
+let rejectedCount = document.getElementById("rejected-count")
+// console.log(interviewCount);
+
+let allFilterBtn = document.getElementById("all-filter-button");
+let interviewFilterBtn = document.getElementById("interview-filter-btn");
+let rejectedFilterBtn = document.getElementById("rejected-filter-button");
+// console.log(interviewFilterBtn)
+// console.log(allFilterBtn)
+
+let allCardSection = document.getElementById('all-cards');
+// console.log(allCardSection);
+let filtersSection = document.getElementById('filter-section');
+let mainContainer = document.querySelector('main');
+console.log(mainContainer);
