912ea86e52538556ac36c07d5f7119becbb025fe
tanzid-48 <mdtanzid.525@gmail.com>
2026-02-22 13:01:01 +0600

    Add toggleStyle function that filter interview and rejected

--- script.js
+++ script.js
@@ -14,39 +14,37 @@ let rejectedFilterBtn = document.getElementById("rejected-filter-button");
 // console.log(allFilterBtn)
 
 let allCardSection = document.getElementById('all-cards');
+let filterButtons = document.getElementById('filter-buttons');
 // console.log(allCardSection);
 let filtersSection = document.getElementById('filter-section');
 let mainContainer = document.querySelector('main');
-console.log(mainContainer);
+// console.log(mainContainer);
 
  function toggleStyle(id){
-    // inactive all first
-        allFilterBtn.classList.remove('bg-black', 'text-white');
-        interviewFilterBtn.classList.remove('bg-black', 'text-white');
-        rejectedFilterBtn.classList.remove('bg-black', 'text-white');
+   // reset all buttons
+   allFilterBtn.classList.remove('bg-blue-500', 'text-white');
+  interviewFilterBtn.classList.remove('bg-blue-500', 'text-white');
+  rejectedFilterBtn.classList.remove('bg-blue-500', 'text-white');
 
-        allFilterBtn.classList.add('bg-blue-500', 'text-white');
-        interviewFilterBtn.classList.add('bg-blue-500', 'text-white');
-        rejectedFilterBtn.classList.add('bg-blue-500', 'text-white');
+  allFilterBtn.classList.add('bg-gray-300', 'text-black');
+  interviewFilterBtn.classList.add('bg-gray-300', 'text-black');
+  rejectedFilterBtn.classList.add('bg-gray-300', 'text-black');
 
-        // after click active the button
+    // active button
       let activeBtn = document.getElementById(id);
-         activeBtn.classList.remove('bg-gray-300', 'text-black');
+    activeBtn.classList.remove('bg-gray-300', 'text-black');
     activeBtn.classList.add('bg-blue-500', 'text-white');
-
-    if(id === "interview-filter-btn"){
-
+       
+    
+    if (id === "interview-filter-btn") {
         allCardSection.classList.remove("hidden");
         filtersSection.classList.add("hidden");
         renderInterview();
-        
-    }else if (id === "rejected-filter-button"){
-
+    } else if (id === "rejected-filter-button") {
         allCardSection.classList.remove("hidden");
         filtersSection.classList.add("hidden");
         renderRejected();
-
-    }else if (id === "all-filter-button"){
+    } else if (id === "all-filter-button") {
         allCardSection.classList.remove("hidden");
         filtersSection.classList.add("hidden");
     }
@@ -55,10 +53,36 @@ console.log(mainContainer);
    // Calculated cardCount 
 
     function calculatedCount(){
+        
+    // total card count
         total.innerText = allCardSection.children.length;
+        
+    // interview count
         interviewCount.innerText = interviewList.length;
         rejectedCount.innerText = rejectedList.length;
     }
     calculatedCount();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  function renderInterview(){
+//     console.log("Interview filter clicked");
+// }
+
+// function renderRejected(){
+//     console.log("Rejected filter clicked");
+// }
 
     
