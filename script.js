54d1808ed7c8670eb0bd58f0008a1633d8740987
tanzid-48 <mdtanzid.525@gmail.com>
2026-02-22 17:58:20 +0600

    add mainContainer event and Interview Button Click

--- script.js
+++ script.js
@@ -20,22 +20,22 @@ let filtersSection = document.getElementById('filter-section');
 let mainContainer = document.querySelector('main');
 // console.log(mainContainer);
 
- function toggleStyle(id){
-   // reset all buttons
-   allFilterBtn.classList.remove('bg-blue-500', 'text-white');
-  interviewFilterBtn.classList.remove('bg-blue-500', 'text-white');
-  rejectedFilterBtn.classList.remove('bg-blue-500', 'text-white');
+function toggleStyle(id) {
+    // reset all buttons
+    allFilterBtn.classList.remove('bg-blue-500', 'text-white');
+    interviewFilterBtn.classList.remove('bg-blue-500', 'text-white');
+    rejectedFilterBtn.classList.remove('bg-blue-500', 'text-white');
 
-  allFilterBtn.classList.add('bg-gray-300', 'text-black');
-  interviewFilterBtn.classList.add('bg-gray-300', 'text-black');
-  rejectedFilterBtn.classList.add('bg-gray-300', 'text-black');
+    allFilterBtn.classList.add('bg-gray-300', 'text-black');
+    interviewFilterBtn.classList.add('bg-gray-300', 'text-black');
+    rejectedFilterBtn.classList.add('bg-gray-300', 'text-black');
 
     // active button
-      let activeBtn = document.getElementById(id);
+    let activeBtn = document.getElementById(id);
     activeBtn.classList.remove('bg-gray-300', 'text-black');
     activeBtn.classList.add('bg-blue-500', 'text-white');
-       
-    
+
+
     if (id === "interview-filter-btn") {
         allCardSection.classList.remove("hidden");
         filtersSection.classList.add("hidden");
@@ -48,20 +48,65 @@ let mainContainer = document.querySelector('main');
         allCardSection.classList.remove("hidden");
         filtersSection.classList.add("hidden");
     }
+}
+
+// Calculated cardCount 
+
+function calculatedCount() {
+
+    // total card count
+    total.innerText = allCardSection.children.length;
+    // total-job
+    let totalJob = document.getElementById('total-job');
+    totalJob.innerText = allCardSection.children.length;
+
+    // interview count
+    interviewCount.innerText = interviewList.length;
+    rejectedCount.innerText = rejectedList.length;
+}
+calculatedCount();
+
+mainContainer.addEventListener('click', function (event) {
+
+    let parentNode = event.target.closest('.card');
+    console.log(parentNode);
+    if (!parentNode) return;
+
+
+    let jobTitle = parentNode.querySelector('.job-title').innerText;
+    //    console.log(jobTitle)
+
+    let jobType = parentNode.querySelector('.job-type').innerText;
+    let jobInfo = parentNode.querySelector('.job-info').innerText;
+    let jobP = parentNode.querySelector('.job-p').innerText;
+   
+
+    //  Interview Button Click
+ if(event.target.classList.contains('interview-btn')){
+
+    parentNode.querySelector('.status').innerText = "Interview";
+
+    let cardInfo = {
+        jobTitle,
+        jobType,
+        jobInfo,
+         status: "Interview",
+         jobP
+    };
+    //  exist check
+    let JobExist = interviewList.find(item => item. jobTitle === jobTitle);
+ if(!JobExist){
+    interviewList.push(cardInfo);
+ }
+//  rejected theke remove
+
+ rejectedList = rejectedList.filter(item => item. jobTitle !== jobTitle);
+
+ calculatedCount();
+ renderInterview();
+
  }
 
-   // Calculated cardCount 
-
-    function calculatedCount(){
-        
-    // total card count
-        total.innerText = allCardSection.children.length;
-        
-    // interview count
-        interviewCount.innerText = interviewList.length;
-        rejectedCount.innerText = rejectedList.length;
-    }
-    calculatedCount();
 
 
 
@@ -77,12 +122,19 @@ let mainContainer = document.querySelector('main');
 
 
 
-//  function renderInterview(){
-//     console.log("Interview filter clicked");
-// }
+});
 
-// function renderRejected(){
-//     console.log("Rejected filter clicked");
-// }
 
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
